c2fa3ecbb28900601bae21e0d6a839b9119d63bf
Tomas Martins <tomfmart@gmail.com>
2020-01-30 18:37:32 -0300

    Initial Commit

---
+++ PMA macOS/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  PMA macOS
+//
+//  Created by Tomas Martins on 30/01/20.
+//  Copyright © 2020 Tomas Martins. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
